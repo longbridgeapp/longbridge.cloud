4851630e9ca0ac3e61c9f4241d8d89a1f472154f
ihavecoke <lb563@foxmail.com>
2022-05-09 22:17:58 +0800

    Add public asset path config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const webpack = require('./config/webpack')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack,
+  assetPrefix: process.env.PUBLIC_ASSET_PATH,
   reactStrictMode: true,
 }
 
