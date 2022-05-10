d26b86f7da1e357caa19090d08c44e3e3067a2bb
ihavecoke <lb563@foxmail.com>
2022-05-10 14:04:03 +0800

    Add index page tdk info

--- utils/seo.tsx
+++ utils/seo.tsx
@@ -1,38 +1,24 @@
-import { NextSeo, NextSeoProps } from "next-seo";
-import React, { FC } from "react";
+import { NextSeo, NextSeoProps } from 'next-seo'
+import React, { FC } from 'react'
 
-export const SEOMeta: FC<NextSeoProps & { url?: string }> = (props) => {
-  const {
-    title: rawTitle,
-    description,
-    url = "https://longbridge.cloud",
-  } = props;
-  const title =
-    rawTitle === "default"
-      ? "Longbridge Whale"
-      : `${rawTitle} - Longbridge Whale`;
+export const SEOMeta: FC<NextSeoProps & { url?: string; indexTitle?: boolean }> = props => {
+  const { title: rawTitle, description, url = 'https://longbridge.cloud', indexTitle } = props
+  const title = indexTitle ? rawTitle : `${rawTitle} - Longbridge Whale`
   const openGraph = {
     title,
     description,
     url,
     images: [
       {
-        url: "https://pub.lbkrs.com/files/202205/bHQKu1TbTP5prjuH/preview.png",
+        url: 'https://pub.lbkrs.com/files/202205/bHQKu1TbTP5prjuH/preview.png',
       },
     ],
-    site_name: "Longbridge Whale",
-  };
+    site_name: 'Longbridge Whale',
+  }
   const twitter = {
-    handle: "@longbridgeapp",
+    handle: '@longbridgeapp',
     site: url,
-    cardType: "summary_large_image",
-  };
-  return (
-    <NextSeo
-      title={title}
-      description={description}
-      openGraph={openGraph}
-      twitter={twitter}
-    />
-  );
-};
+    cardType: 'summary_large_image',
+  }
+  return <NextSeo title={title} description={description} openGraph={openGraph} twitter={twitter} />
+}
