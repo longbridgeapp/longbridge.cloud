ff1f7f21f46a5f069c5b7f32ff52962b39e604c5
于跃 <yue.yu_@longbridge.sg>
2022-05-11 10:23:38 +0800

    调整文案标点和部分样式

--- features/about/gallery.tsx
+++ features/about/gallery.tsx
@@ -9,7 +9,7 @@ export const AboutGallery = () => {
     <div className="main-container py-[120px]">
       <div className={classNames('main-content-width')}>
         <h3 className="text-4xl">
-          <span className="font-bold">{i18n.t('about_gallery_001')}</span>
+          <span className="font-bold">{i18n.t('about_gallery_001')}</span>&nbsp;
           <span className="font-normal">{i18n.t('about_gallery_002')}</span>
         </h3>
         <div className="mt-8">
