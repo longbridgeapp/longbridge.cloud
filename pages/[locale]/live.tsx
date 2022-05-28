32ea8bd9b74a1efdc2e7a08d10d1594614d3ee93
ihavecoke <lb563@foxmail.com>
2022-05-28 17:43:26 +0800

    Add Live page meta info

--- pages/[locale]/live.tsx
+++ pages/[locale]/live.tsx
@@ -3,6 +3,8 @@ import Live from '@/features/live'
 import { i18nPaths } from '@/utils/i18n-paths'
 import i18nextConfig from '../../next-i18next.config'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import {useTranslation} from "next-i18next";
+import { SEOMeta } from '@/utils/seo'
 
 export const getStaticPaths = () => ({
   fallback: false,
@@ -15,6 +17,11 @@ export const getStaticProps = async (ctx: any) => ({
 })
 
 const LivePage = () => {
-  return <Live />
+  const seoI18n = useTranslation(['seo'])
+  const i18n = useTranslation('common')
+  return <>
+    <SEOMeta title={seoI18n.t('live.title')} description={seoI18n.t('live.description')} />
+    <Live />
+  </>
 }
 export default LivePage
