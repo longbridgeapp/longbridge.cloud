70a3d0ac565b0fd84c905586cae553f1a90fcfed
yuanchangrui <hold_baby@qq.com>
2022-07-22 19:43:18 +0800

    feat: 文案更新

--- features/block/top-banner.tsx
+++ features/block/top-banner.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-i18next'
 import { Block, BlockHeader, BlockBetween, TopBlock } from '@/components/block'
 import { TalkToUs } from '@/features/talk-to-us'
+import DivideDot from '@/components/divide-dot'
 
 export const BG_BANNER = 'https://pub.lbkrs.com/files/202207/g1pGdC8PkwxTaqmw/trading_platform.png'
 const IMAGE_URL = 'https://pub.lbkrs.com/files/202207/Ggr6CvK2f2jWASHe/Group_427318855.png'
@@ -68,6 +69,35 @@ export const WhiteLabelBanner = () => {
             </>
           }
           right={<img src={IMAGE_URL} className="mt-5 lg:mt-0" alt={i18n.t('top-banner.white-label-title')} />}
+        />
+      </Block>
+    </TopBlock>
+  )
+}
+
+export const IndexBanner = () => {
+  const i18n = useTranslation('common')
+  return (
+    <TopBlock imageUrl={BG_BANNER}>
+      <Block>
+        <BlockBetween
+          left={
+            <>
+              <div className="text-[50px] font-medium">
+                <span>{i18n.t('home_page_002')}</span>&nbsp;
+                <span className="text-text_brand_color">{i18n.t('home_page_001')}</span>
+              </div>
+              <DivideDot size="large" />
+              <p className="mt-6 text-xl">{i18n.t('home_page_003')}</p>
+            </>
+          }
+          right={
+            <img
+              src={'https://pub.lbkrs.com/files/202207/yGQxErdGoofNJhoh/header_img.png'}
+              className="mt-5 lg:mt-0"
+              alt={i18n.t('top-banner.broker-title')}
+            />
+          }
         />
       </Block>
     </TopBlock>
--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { HomeFeatures } from '@/features/home/features'
-import DivideDot from '@/components/divide-dot'
 import { HomeExamples } from '@/features/home/examples'
 import { i18nPaths } from '@/utils/i18n-paths'
 import i18nextConfig from '@/next-i18next.config'
 import { SEOMeta } from '@/utils/seo'
-import { PageLayout } from '@/features/common/page-layout'
-import { BG_BANNER } from '@/features/block/top-banner'
+import { Layout } from '@/features/common/page-layout'
+import { IndexBanner } from '@/features/block/top-banner'
 
 export const getStaticPaths = () => ({
   fallback: false,
@@ -21,31 +20,14 @@ export const getStaticProps = async (ctx: any) => ({
 })
 const IndexPage = () => {
   const seoI18n = useTranslation(['seo'])
-  const i18n = useTranslation(['common'])
-
-  const title = (
-    <>
-      <div>
-        <span>{i18n.t('home_page_002')}</span>&nbsp;
-        <span className="text-text_brand_color">{i18n.t('home_page_001')}</span>
-      </div>
-      <DivideDot size="large" />
-    </>
-  )
 
   return (
-    <PageLayout
-      screenProps={{
-        backgroundImage: `url(${BG_BANNER})`,
-        title,
-        desc: i18n.t('home_page_003'),
-        cover: 'https://pub.lbkrs.com/files/202207/yGQxErdGoofNJhoh/header_img.png',
-      }}
-    >
+    <Layout>
       <SEOMeta indexTitle={true} title={seoI18n.t('tdk.title')} description={seoI18n.t('tdk.description')} />
+      <IndexBanner />
       <HomeFeatures />
       <HomeExamples />
-    </PageLayout>
+    </Layout>
   )
 }
 export default IndexPage
