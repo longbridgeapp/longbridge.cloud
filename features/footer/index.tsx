c3ce32f40510933bf9980f6cebf9ef619d1e1629
libo <bo.li@longbridge.sg>
2022-11-09 16:26:35 +0800

    针对 CN 站点显示备案信息

--- features/footer/index.tsx
+++ features/footer/index.tsx
@@ -1,12 +1,15 @@
 import classNames from 'classnames'
-import { FC, useMemo } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './index.module.scss'
+import { useMount } from 'ahooks'
 
 const email = 'cs@longbridge.cloud'
 const tel = ''
 
 const Footer: FC = () => {
+  const [isCN, setISCN] = useState(false)
+
   const i18n = useTranslation('common')
   const socials = useMemo(() => {
     return [
@@ -34,6 +37,11 @@ const Footer: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useMount(() => {
+    const isCN = window.location.hostname.includes('.cn')
+    setISCN(isCN)
+  })
+
   return (
     <footer className={classNames(styles.footer, 'main-container')}>
       <div className={classNames('main-content-width')}>
@@ -42,28 +50,40 @@ const Footer: FC = () => {
             {false && (
               <div className="flex flex-col">
                 <span className="text-sm">{i18n.t('footer_004')}</span>
-                <a className="mt-2 font-medium" href={`tel:${tel}`}>{tel}</a>
+                <a className="mt-2 font-medium" href={`tel:${tel}`}>
+                  {tel}
+                </a>
               </div>
             )}
             <div className="flex flex-col">
               <span className="text-sm">{i18n.t('footer_003')}</span>
-              <a className="mt-2 font-medium" href={`mailto:${email}`}>{email}</a>
+              <a className="mt-2 font-medium" href={`mailto:${email}`}>
+                {email}
+              </a>
             </div>
           </div>
           <div>
-            {false && socials.map(({ icon, href }) => {
-              return (
-                <a className="w-5 inline-block ml-7 first:ml-0" href={href} key={href}>
-                  <img src={icon} alt="" />
-                </a>
-              )
-            })}
+            {false &&
+              socials.map(({ icon, href }) => {
+                return (
+                  <a className="w-5 inline-block ml-7 first:ml-0" href={href} key={href}>
+                    <img src={icon} alt="" />
+                  </a>
+                )
+              })}
           </div>
         </div>
         <div className="flex justify-between items-center mt-6">
           <div className="text-xs">
             <span>{i18n.t('footer_005')}</span>
           </div>
+          {isCN && (
+            <div className="text-xs">
+              <a href="https://beian.miit.gov.cn" target={'_blank'}>
+                浙 ICP 备 19021593 号
+              </a>
+            </div>
+          )}
           {false && (
             <div>
               {links.map(({ label, href }) => {
