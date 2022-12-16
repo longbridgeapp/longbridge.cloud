0ab71d3d161292190d3b1d4ca1014d375a4492fc
ylinwind <ylinwind@163.com>
2022-12-16 17:13:26 +0800

    feat: 增加媒体报道配置 page

---
+++ features/whale-reports/list.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useReports } from '@/hooks/use-reports'
+import { ReportItem } from '@/features/whale-reports/types'
+import styles from './list.module.scss'
+
+export const WhaleReportList: FC = () => {
+  const [reports] = useReports()
+  const i18n = useTranslation('common')
+
+  return (
+    <div className={`${styles['whale-report-list']} main-content-width main-container pt-[100px]`}>
+      <div className="report-title">{i18n.t('header_nav_009')}</div>
+      {reports.map((report, index) => (
+        <WhaleReportItem key={index} report={report} />
+      ))}
+    </div>
+  )
+}
+
+export const WhaleReportItem = ({ report }: { report: ReportItem }) => {
+  return (
+    <div className="report-wrapper">
+      <div className="report">
+        <div className="stamp">{report.time}</div>
+        <div className="title">
+          {report.source_link ? (
+            <a href={report.source_link} target="_blank" rel="noreferrer">
+              {report.title}
+            </a>
+          ) : (
+            report.title
+          )}
+        </div>
+        <div className="desc">{report.description}</div>
+      </div>
+      <img src={report.image} alt="" className="report-image" />
+    </div>
+  )
+}
