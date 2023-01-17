85e0a7417c003174d5919ccdf490ac1a64fbaf4f
佳成 <jiacheng.zhou@longbridge.sg>
2023-01-17 22:06:28 +0800

    feat: 完成投放系统走查修改

--- pages/[locale]/delivery-system.tsx
+++ pages/[locale]/delivery-system.tsx
@@ -12,6 +12,7 @@ import { SEOMeta } from '@/utils/seo'
 import { useDebounceEffect, useSize } from 'ahooks'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Banner from '@/features/product/Banner'
 
 export const getStaticPaths = () => ({
   fallback: false,
@@ -25,8 +26,8 @@ export const getStaticProps = async (ctx: any) => ({
 })
 
 const DeliverySystem: React.FC = () => {
-  const i18n = useTranslation('common')
   const seoI18n = useTranslation(['seo'])
+  const i18n = useTranslation('common')
   const isEN = i18n.i18n.language === 'en'
   const marketSceneRef = useRef(null)
   const marketSceneSize = useSize(marketSceneRef)
@@ -85,30 +86,21 @@ const DeliverySystem: React.FC = () => {
     [marketSceneSize?.width],
     { wait: 300 }
   )
+
+  const banner_porps = {
+    title: i18n.t('whale-delivery-system-001'),
+    desc: [i18n.t('whale-delivery-system-002'), i18n.t('whale-delivery-system-003')],
+    img: {
+      'en': 'https://assets.lbkrs.com/uploads/c29968f5-9d36-4de0-a644-967dc61b4472/Untitled-1.png',
+      'zh-CN': 'https://assets.lbkrs.com/uploads/c29968f5-9d36-4de0-a644-967dc61b4472/Untitled-1.png',
+      'zh-HK': 'https://assets.lbkrs.com/uploads/c29968f5-9d36-4de0-a644-967dc61b4472/Untitled-1.png',
+    },
+  }
   return (
     <Layout>
       <SEOMeta indexTitle={true} title={seoI18n.t('tdk.title')} description={seoI18n.t('tdk.description')} />
       <div>
-        <div
-          className="py-10 bg-cover main-container lg:py-20"
-          style={{ backgroundImage: `url('${CDN_IMAGES.banner_bg}')` }}
-        >
-          <div className="flex flex-col gap-8 main-content-width lg:flex-row">
-            <div className="flex flex-col items-start flex-1 gap-4 lg:gap-8 content">
-              <div className="text-3xl xl:text-[44px] font-semibold mt-0 xl:mt-5">
-                {i18n.t('whale-delivery-system-001')}
-              </div>
-              <div className="text-lg leading-9 text-text_color_2 max-w-[500px]">
-                <div>{i18n.t('whale-delivery-system-002')}</div>
-                <div>{i18n.t('whale-delivery-system-003')}</div>
-              </div>
-              <TalkToUs className="mt-0" />
-            </div>
-            <div className="flex-1">
-              <img src="https://assets.lbkrs.com/uploads/c29968f5-9d36-4de0-a644-967dc61b4472/Untitled-1.png" alt="" />
-            </div>
-          </div>
-        </div>
+        <Banner {...banner_porps}></Banner>
         <div className="py-10 main-container lg:py-20">
           <div className="flex flex-col gap-10 main-content-width">
             <div>
@@ -120,7 +112,7 @@ const DeliverySystem: React.FC = () => {
                 <div className="flex flex-col flex-1 overflow-hidden border rounded-lg border-border_color" key={index}>
                   <div className="flex flex-col scene-first">
                     <div className="text-[22px] p-8 pb-0 font-medium mb-2">{item.title}</div>
-                    <div className="flex-1 pb-5 mx-8 text-sm font-normal border-b border-dashed border-tag_border_color">
+                    <div className="flex-1 pb-5 mx-8 text-sm font-normal border-b border-dashed border-tag_border_color text-text_color_2">
                       {item.titleTips}
                     </div>
                   </div>
@@ -155,14 +147,14 @@ const DeliverySystem: React.FC = () => {
               <div className="flex-1 p-10">
                 <div className="border-b border-dashed version-first border-tag_border_color">
                   <div className="text-[30px] font-semibold mb-2">{i18n.t('whale-delivery-system-030')}</div>
-                  <div className="text-base font-normal pb-7">
+                  <div className="text-base font-normal pb-7 text-text_color_1_supplement">
                     <div>{i18n.t('whale-delivery-system-031')}</div>
                     <div className="h-6"></div>
                   </div>
                 </div>
                 <div className="version-second">
                   <div className="text-xl font-medium mt-[30px] mb-[10px]">{i18n.t('whale-delivery-system-032')}</div>
-                  <ul className="flex flex-col gap-y-4">
+                  <ul className="flex flex-col gap-y-4 ">
                     <li className="list-dot">{i18n.t('whale-delivery-system-033')}</li>
                     <li className="list-dot">{i18n.t('whale-delivery-system-034')}</li>
                     <li className="list-dot">{i18n.t('whale-delivery-system-035')}</li>
@@ -172,7 +164,7 @@ const DeliverySystem: React.FC = () => {
               <div className="flex-1 p-10 border-l bg-bg_color_2 border-border_color">
                 <div className="border-b border-dashed version-first border-tag_border_color">
                   <div className="text-[30px] font-semibold mb-2">{i18n.t('whale-delivery-system-036')}</div>
-                  <div className="text-base font-normal pb-7">
+                  <div className="text-base font-normal pb-7 text-text_color_1_supplement">
                     <div>{i18n.t('whale-delivery-system-037')}</div>
                     <div>{i18n.t('whale-delivery-system-038')}</div>
                   </div>
@@ -194,7 +186,7 @@ const DeliverySystem: React.FC = () => {
                       HOT
                     </span>
                   </div>
-                  <div className="text-base font-normal pb-7 ">
+                  <div className="text-base font-normal pb-7 opacity-80">
                     <div>{i18n.t('whale-delivery-system-043')}</div>
                     <div>{i18n.t('whale-delivery-system-044')}</div>
                   </div>
@@ -251,44 +243,54 @@ const DeliverySystem: React.FC = () => {
               <div className="mb-2 text-base text-brand_color">{i18n.t('whale-delivery-system-056')}</div>
               <div className="text-2xl font-medium xl:text-4xl">{i18n.t('whale-delivery-system-057')}</div>
             </div>
-            <div className="flex flex-col justify-between gap-10 lg:items-center lg:gap-40 lg:flex-row">
+            <div className="flex flex-col justify-between gap-10 lg:items-center lg:flex-row">
               <div className="flex-1">
                 <img
                   src="https://assets.lbkrs.com/uploads/1e82d4e2-ca8f-463b-860a-700df8d3a8cb/Frame 427319007.png"
                   alt=""
-                  className="lg:w-[530px]"
+                  className="lg:w-[530px] lg:h-[320px]"
                 />
               </div>
-              <div className="flex flex-col flex-1 gap-4">
+              <div className="flex flex-col flex-1 gap-5 max-w-[580px]">
                 <div className="text-2xl lg:text-[28px] font-medium">{i18n.t('whale-delivery-system-058')}</div>
-                <div className="flex items-center gap-5 ">
-                  <div className="text-base font-medium w-[100px]">{i18n.t('whale-delivery-system-059')}</div>
-                  <div className="flex-1 text-base font-normal text-text_color_3">
-                    {i18n.t('whale-delivery-system-060')}
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center gap-5 ">
+                    <div className="text-base font-medium w-[103px] leading-[22px]">
+                      {i18n.t('whale-delivery-system-059')}
+                    </div>
+                    <div className="flex-1 text-base font-normal text-text_color_3 leading-[22px]">
+                      {i18n.t('whale-delivery-system-060')}
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center gap-5 ">
-                  <div className="text-base font-medium w-[100px]">{i18n.t('whale-delivery-system-061')}</div>
-                  <div className="flex-1 text-base font-normal text-text_color_3">
-                    {i18n.t('whale-delivery-system-062')}
+                  <div className="flex items-center gap-5 ">
+                    <div className="text-base font-medium w-[103px] leading-[22px]">
+                      {i18n.t('whale-delivery-system-061')}
+                    </div>
+                    <div className="flex-1 text-base font-normal text-text_color_3 leading-[22px]">
+                      {i18n.t('whale-delivery-system-062')}
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center gap-5 ">
-                  <div className="text-base font-medium w-[100px]">{i18n.t('whale-delivery-system-063')}</div>
-                  <div className="flex-1 text-base font-normal text-text_color_3">
-                    {i18n.t('whale-delivery-system-064')}
+                  <div className="flex items-center gap-5 ">
+                    <div className="text-base font-medium w-[103px] leading-[22px]">
+                      {i18n.t('whale-delivery-system-063')}
+                    </div>
+                    <div className="flex-1 text-base font-normal text-text_color_3 leading-[22px]">
+                      {i18n.t('whale-delivery-system-064')}
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center gap-5 ">
-                  <div className="text-base font-medium w-[100px]">{i18n.t('whale-delivery-system-065')}</div>
-                  <div className="flex-1 text-base font-normal text-text_color_3">
-                    {i18n.t('whale-delivery-system-066')}
+                  <div className="flex items-center gap-5 ">
+                    <div className="text-base font-medium w-[103px] leading-[22px]">
+                      {i18n.t('whale-delivery-system-065')}
+                    </div>
+                    <div className="flex-1 text-base font-normal text-text_color_3 leading-[22px]">
+                      {i18n.t('whale-delivery-system-066')}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div>
-              <div className="mb-2 text-sm text-brand_color">{i18n.t('whale-delivery-system-067')}</div>
+              <div className="mb-3 text-sm text-brand_color">{i18n.t('whale-delivery-system-067')}</div>
               <div className="p-8 rounded-lg bg-bg_color_2">
                 <div className="flex flex-col justify-between gap-3 xl:flex-row">
                   <div className="flex flex-col justify-between flex-1 w-full gap-12 md:flex-row">
