f54a88234f14ae9828945875de4762494cb95600
佳成 <jiacheng.zhou@longbridge.sg>
2023-05-26 17:42:07 +0800

    feat: 增加 Crypto 上线预热 页面

--- pages/[locale]/virtual-asset-trading-system.tsx
+++ pages/[locale]/virtual-asset-trading-system.tsx
@@ -1,21 +1,41 @@
-import Head from 'next/head'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import i18nextConfig from '@/next-i18next.config'
+import { NextSeo } from 'next-seo'
+import { i18nPaths } from '@/utils/i18n-paths'
+
+export const getStaticPaths = () => ({
+  fallback: false,
+  paths: i18nPaths(),
+})
+
+export const getStaticProps = async (ctx: any) => ({
+  props: {
+    ...(await serverSideTranslations(ctx?.params?.locale, ['common', 'seo'], i18nextConfig as unknown as any)),
+  },
+})
 
 const VirtualAssetTradingSystem = () => {
   return (
     <>
-      <Head>
-        <title>7x24 一站式虛擬資產 交易解決方案</title>
-        <meta property="og:title" content="Whale 將推出全港首個面向券商的"></meta>
-        <meta property="og:description" content="7x24 一站式虛擬資產 交易解決方案"></meta>
-        <meta
-          property="og:image"
-          content="https://pub.lbkrs.com/files/202305/smtJobvVG139kuLz/20230526-150057.png"
-        ></meta>
-        <meta
-          property="og:url"
-          content="https://activity.lbkrs.com/pages/longbridge/1968/index.html?app_id=longbridge&amp;org_id=1&amp;account_channel=lb"
-        ></meta>
-      </Head>
+      <NextSeo
+        title={'7x24 一站式虛擬資產 交易解決方案'}
+        description={'7x24 一站式虛擬資產 交易解決方案'}
+        openGraph={{
+          title: 'Whale 將推出全港首個面向券商的',
+          description: '7x24 一站式虛擬資產 交易解決方案',
+          url: 'https://activity.lbkrs.com/pages/longbridge/1968/index.html?app_id=longbridge&org_id=1&account_channel=lb',
+          images: [
+            {
+              url: 'https://pub.lbkrs.com/files/202305/smtJobvVG139kuLz/20230526-150057.png',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@longbridgeapp',
+          site: 'https://activity.lbkrs.com/pages/longbridge/1968/index.html?app_id=longbridge&org_id=1&account_channel=lb',
+          cardType: 'summary_large_image',
+        }}
+      />
       <iframe
         src="https://activity.lbkrs.com/pages/longbridge/1968/index.html?app_id=longbridge&org_id=1&account_channel=lb"
         width="100%"
