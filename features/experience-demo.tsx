f3bf019a1f788e8d71a674d05a8c070753bed84f
Jason Lee <huacnlee@gmail.com>
2023-10-29 16:19:46 +0800

    修正体验飞书表单在 iframe 里面需要登录的问题。改为新窗口打开

--- features/experience-demo.tsx
+++ features/experience-demo.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import { useTranslation } from 'next-i18next'
-import { FullMask } from '@/components/popup'
 import Button from '@/components/button'
 import Icon from '@/components/icon'
+import { FullMask } from '@/components/popup'
 import classnames from 'classnames'
+import { useTranslation } from 'next-i18next'
+import { useState } from 'react'
 
 const formInfo: any = {
   'zh-HK': 'https://longbridge.feishu.cn/share/base/form/shrcnhxdx8hRz3BYtrdTA8qO1Vb?lang=zh-HK',
@@ -25,6 +25,10 @@ const ExperienceDemo = ({ className = '' }) => {
   const [visible, setVisible] = useState(false)
   const i18n = useTranslation('common')
 
+  const openForm = () => {
+    window.open(formInfo[i18n.i18n.language])
+  }
+
   return (
     <>
       {visible && (
@@ -45,7 +49,7 @@ const ExperienceDemo = ({ className = '' }) => {
           '!bg-transparent cursor-pointer mt-10 border border-brand_color !text-brand_color h-[42px] px-2.5 flex items-center justify-center hover:text-brand_color_2 hover:border-brand_color_2',
           className
         )}
-        onClick={() => setVisible(true)}
+        onClick={openForm}
       >
         {i18n.t('features_experience_demo_891117')}
       </Button>
